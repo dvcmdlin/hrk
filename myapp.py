e1f81c56e2db4f23a495165d3b5bc323bbc1341f
david <david11092014@gmail.com>
2021-02-28 08:46:01 +0800

    make it better

--- myapp.py
+++ myapp.py
@@ -1,18 +1,18 @@
 
 from flask import Flask
 
-myapp=Flask(__name__)
+app=Flask(__name__)
 
-@myapp.route("/")
+@app.route("/")
 def home():
     # Use a breakpoint in the code line below to debug your script.
     return "hi ! my Flask only"  # Press Ctrl+F8 to toggle the breakpoint.
 
-@myapp.route("/tst")
+@app.route("/tst")
 def tst():
     return "hi ! my Flask tst"
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    myapp.run()
+    app.run()
     # print_hi('PyCharm')
